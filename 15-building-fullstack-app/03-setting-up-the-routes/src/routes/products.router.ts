bc3c162d61811d42bd6089318459ea586719a600
Chillie <reskialamsyah@gmail.com>
2025-11-07 13:09:19 +0800

    UPDATE: finish subsection 3 setting up the routes

---
+++ 15-building-fullstack-app/03-setting-up-the-routes/src/routes/products.router.ts
@@ -0,0 +1,20 @@
+/*
+Challenge 1:
+
+  - Use express.Router() to export a router called productsRouter
+
+   It should mount the '/api/products' and '/api/products/genres' routes.
+   These should use the two functions from productsControllers.js: getProducts() and getGenres().
+   Be careful here - what is the common pitfall?
+
+   hint.md for help!
+*/
+
+import express from "express";
+import { getProducts, getGenres } from "../controllers/products.controller";
+
+export const productRouter = express.Router();
+
+productRouter.get("/products", getProducts);
+
+productRouter.get("/products/genres", getGenres);
