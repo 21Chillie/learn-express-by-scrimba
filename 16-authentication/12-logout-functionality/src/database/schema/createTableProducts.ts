1989b580a96e3c71709a174e9cf0837ea675fa8f
Chillie <reskialamsyah@gmail.com>
2025-11-09 02:56:18 +0800

    UPDATE: finish subsection 12 logout functionality

---
+++ 16-authentication/12-logout-functionality/src/database/schema/createTableProducts.ts
@@ -0,0 +1,23 @@
+import { getDatabaseConnection } from "./init.database";
+
+function createTable() {
+	const db = getDatabaseConnection();
+
+	db.prepare(
+		`
+  CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    price REAL NOT NULL,
+    image TEXT NOT NULL,
+    year INTEGER,
+    genre TEXT,
+    stock INTEGER
+  )
+  `
+	).run();
+	console.log("Table 'products' initialized");
+}
+
+createTable();
